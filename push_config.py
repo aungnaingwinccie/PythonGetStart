3d2e1ee4a4462ed421efcf885fe6b49dfe68b52b
Win <61113323+aungnaingwinccie@users.noreply.github.com>
2023-02-01 17:04:01 +0800

    Update push_config.py
    Change variable name

--- push_config.py
+++ push_config.py
@@ -1,14 +1,14 @@
 from netmiko import ConnectHandler
 
-IP = 0
-ip_addr = ["192.168.26.10", "192.168.26.20"]
+ip_range = 0
+iplist = ["192.168.26.10", "192.168.26.20"]
 
-for iplist in ip_addr:
-    IP = IP + 1
+for ip_addr in iplist:
+    ip_range = ip_range + 1
     # Set the device information and credentials
     device = {
         "device_type": "cisco_ios",
-        "ip": iplist,
+        "ip": ip_addr,
         "username": "cisco",
         "password": "cisco",
     }
@@ -18,8 +18,8 @@ for iplist in ip_addr:
     
     # Create a list of commands to configure loopback interface
     commands = [
-        "interface loopback "+str(IP),
-        "ip address 172.16."+str(IP)+".1"
+        "interface loopback "+str(ip_range),
+        "ip address 172.16."+str(ip_range)+".1"
         " 255.255.255.0",
         "end",
         "write memory",
