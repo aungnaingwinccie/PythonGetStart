04d37d2436322a8d7c8e80e92a3e0a1a932430fb
Win <61113323+aungnaingwinccie@users.noreply.github.com>
2023-02-02 16:33:24 +0800

    Update config_log.py

--- config_log.py
+++ config_log.py
@@ -1,3 +1,5 @@
+#Taking log or configuration from Router or Switch.
+
 from netmiko import ConnectHandler
 
 # Set the device information and credentials
@@ -8,13 +10,15 @@ device = {
     "password": "cisco",
 }
 
-# Connect to the device
+# Connecting to the device
 net_connect = ConnectHandler(**device)
 
 # Execute the "show running-config" command and store the output
 output = net_connect.send_command("show running-config")
 
 # Write the output to a file
+# Using "with" no need to close the file back
+
 with open("running_config.txt", "w") as file:
     file.write(output)
 
